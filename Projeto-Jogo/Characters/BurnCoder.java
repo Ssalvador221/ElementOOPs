cbb86a73d6737c0ebe1d9fd811c19e93d4177086
Ssalvador221 <j8731615@gmail.com>
2023-10-08 21:08:44 -0300

    New features: Add methods and attributes to Creatures!

--- Projeto-Jogo/Characters/BurnCoder.java
+++ Projeto-Jogo/Characters/BurnCoder.java
@@ -1,5 +1,17 @@
 package Characters;
 
 public class BurnCoder extends Creatures{
+
+    @Override
+    public int elementalDamage(Creatures creatures) {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'elementalDamage'");
+    }
+
+    @Override
+    public String getEspeciePlayer() {
+        // TODO Auto-generated method stub
+        throw new UnsupportedOperationException("Unimplemented method 'getEspeciePlayer'");
+    }
     
 }
--- Projeto-Jogo/Characters/Creatures.java
+++ Projeto-Jogo/Characters/Creatures.java
@@ -1,5 +1,70 @@
-package Characters;
 
-public class Creatures{
+package Characters;
+import java.util.Random;
+
+public abstract class  Creatures {
+    private final int poder; 
+    private final int ataque;
+    private final int defesa;
+    private final int velocidade;
+    private final int pontosDeVida;
+    private int totalVida;
+
+    
+    
+    public Creatures() {
+        this.poder = getRandomValue(10, 30);
+        this.ataque = getRandomValue(5, 15);
+        this.defesa = getRandomValue(5, 15);
+        this.velocidade = getRandomValue(1, 10);
+        this.totalVida = getRandomValue(200, 400);
+        this.pontosDeVida = totalVida;
+
+    }
+
+
+    public static int getRandomValue(int valorMax, int valorMin){
+        Random rand = new Random();
+        return rand.nextInt(valorMax - valorMin + 1) + valorMin;
+    }
+
+    
+    public int getPoder() {
+        return poder;
+    }
+
+    public int getAtaque() {
+        return ataque;
+    }
+
+    public int getDefesa() {
+        return defesa;
+    }
+
+    public int getVelocidade() {
+        return velocidade;
+    }
+
+    public int getPontosDeVida() {
+        return pontosDeVida;
+    }
+
+    public void vidaPlayer() {
+        this.totalVida = this.pontosDeVida;
+    }
+
+    
+    public int attackPhysical(Creatures creatures){
+        return this.getPoder() * this.getAtaque() / creatures.getDefesa();
+    }
+
+    public void damage(int dano){
+        this.totalVida -= dano;
+    }
+
+    public abstract int elementalDamage(Creatures creatures);
+
+    public abstract String getEspeciePlayer();
+
     
 }
